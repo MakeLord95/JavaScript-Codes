1efb3406e2649d9a9962cac23886e522c644ab1d
Make <juhani.markus@gmail.com>
2022-11-04 22:24:04 +0200

    teht_2.1 tehty

---
+++ Kotitehtävät/2/teht_2.1.js
@@ -0,0 +1,30 @@
+let nbr = 1;
+let value;
+let suffix;
+let numbers = [];
+
+function reverseArray(numbers) {
+  let reversedArray = [];
+  for (let i = numbers.length - 1; i >= 0; i--) {
+    reversedArray.push(numbers[i]);
+  }
+  return reversedArray;
+}
+
+while (nbr <= 5) {
+  if (nbr === 1) {
+    suffix = 'st';
+  } else if (nbr === 2) {
+    suffix = 'nd';
+  } else if (nbr === 3) {
+    suffix = 'rd';
+  } else {
+    suffix = 'th';
+  }
+
+  value = parseInt(prompt('Enter the ' + nbr + suffix + ' value'));
+  numbers.push(value);
+  nbr++;
+}
+
+console.log(reverseArray(numbers));
