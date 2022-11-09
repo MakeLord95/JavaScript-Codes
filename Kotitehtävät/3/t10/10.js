6b7d7527b568289d92be5a259f94d9ba91c973d1
Make <juhani.markus@gmail.com>
2022-11-09 03:49:08 +0200

    Moduuli 3, tehtävä 10 done tehtävä 11 WIP

---
+++ Kotitehtävät/3/t10/10.js
@@ -0,0 +1,21 @@
+let first_name;
+let last_name;
+let full_name;
+
+const button = document.querySelector('input[type="submit"]');
+
+button.addEventListener('click', function(evt) {
+  event.preventDefault();
+  console.log('button pressed');
+
+  first_name = document.querySelector('input[name="firstname"]').value;
+  last_name = document.querySelector('input[name="lastname"]').value;
+
+  full_name = first_name + ' ' + last_name;
+
+  console.log(first_name);
+  console.log(last_name);
+  console.log(full_name);
+
+  document.getElementById('target').innerHTML = 'Your name is ' + full_name;
+});
--- Kotitehtävät/3/t9/9.js
+++ Kotitehtävät/3/t9/9.js
@@ -1,63 +1,63 @@
+let chars;
 let result;
-let symbol;
 let operand;
 let number_1;
 let number_2;
 
 const button = document.getElementById('start');
 
-button.addEventListener('click', function (evt) {
-    console.log('button pressed')
+button.addEventListener('click', function(evt) {
+  console.log('button pressed');
 
-    let sentence = document.getElementById('calculation').value;
+  let sentence = document.getElementById('calculation').value;
 
-    let add = '+'
-    let sub = '-'
-    let multi21 = '*'
-    let div = '/'
+  let add = '+';
+  let sub = '-';
+  let multi21 = '*';
+  let div = '/';
 
-    for (let i = 0; i < sentence.length; i++) {
-        if (sentence[i].includes(add)) {
-            operand = '+'
-            console.log(sentence[i])
-        } else if (sentence[i].includes(sub)) {
-            operand = '-'
-            console.log(sentence[i])
-        } else if (sentence[i].includes(multi21)) {
-            operand = '*'
-            console.log(sentence[i])
-        } else if (sentence[i].includes(div)) {
-            operand = '/'
-            console.log(sentence[i])
-        } else {
-            number_1 = sentence[i]
-            console.log(sentence[i])
-        }
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i].includes(add)) {
+      chars = sentence.split('+');
+      operand = '+';
+      console.log(sentence[i]);
+    } else if (sentence[i].includes(sub)) {
+      chars = sentence.split('-');
+      operand = '-';
+      console.log(sentence[i]);
+    } else if (sentence[i].includes(multi21)) {
+      chars = sentence.split('*');
+      operand = '*';
+      console.log('multi21 Seb, multi21');
+    } else if (sentence[i].includes(div)) {
+      chars = sentence.split('/');
+      operand = '/';
+      console.log(sentence[i]);
     }
-    /*
-    let chars = sentence.split('');
-    number_1 = chars[0]
-    operand = chars[1]
-    number_2 = chars[2]
+  }
 
-    if (operand === '+') {
-        result = parseFloat(number_1) + parseFloat(number_2)
-        symbol = '+'
-    } else if (operand === '-') {
-        result = parseFloat(number_1) - parseFloat(number_2)
-        symbol = '-'
-    } else if (operand === '*') {
-        result = parseFloat(number_1) * parseFloat(number_2)
-        symbol = '*'
-    } else if (operand === '/') {
-        result = parseFloat(number_1) / parseFloat(number_2)
-        symbol = '/'
-    } else {
-        console.log('Invalid Input!')
-    }
+  number_1 = chars[0];
+  number_2 = chars[1];
+  console.log(number_1);
+  console.log(number_2);
 
-    console.log(number_1, symbol, number_2 + ' = ' + result)
-    document.querySelector('#result').innerHTML = result;
+  if (operand === '+') {
+    result = parseFloat(number_1) + parseFloat(number_2);
+    symbol = '+';
+  } else if (operand === '-') {
+    result = parseFloat(number_1) - parseFloat(number_2);
+    symbol = '-';
+  } else if (operand === '*') {
+    result = parseFloat(number_1) * parseFloat(number_2);
+    symbol = '*';
+  } else if (operand === '/') {
+    result = parseFloat(number_1) / parseFloat(number_2);
+    symbol = '/';
+  } else {
+    console.log('Invalid Input!');
+  }
 
-     */
+  console.log(number_1, symbol, number_2 + ' = ' + result);
+  document.querySelector('#result').innerHTML = result;
+
 });
