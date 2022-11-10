2396eaab21e974309e378f33bbe74a4149db2a4a
Make <juhani.markus@gmail.com>
2022-11-10 11:07:55 +0200

    Moduuli 4 printaa consoleen nyt json-formaatissa tuloksen

--- Kotitehtävät/4/teht_4.x.js
+++ Kotitehtävät/4/teht_4.x.js
@@ -1,25 +1,39 @@
 async function asynchronousFunction(search_term) {
-  console.log('asynchronous download begins');
-  try {
-    const response = await fetch(
-        'https://api.tvmaze.com/search/shows?q=' + search_term);
-    const jsonData = await response.json();
-    console.log(jsonData);
-  } catch (error) {
-    console.log(error.message);
-  } finally {
-    console.log('asynchronous load complete');
-  }
+    console.log('asynchronous download begins');
+    try {
+
+        fetch('https://api.tvmaze.com/search/shows?q=' + search_term)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                appendData(data);
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+
+    } catch (error) {
+        console.log(error.message);
+    } finally {
+        console.log('asynchronous load complete');
+    }
+}
+
+function appendData(data) {
+    console.clear()
+    let jsonData = JSON.stringify(data,null,2)
+    console.log(jsonData)
 }
 
 const button = document.querySelector('input[type="submit"]');
 
-button.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  console.log('button pressed');
 
-  let search_term = 'Futurama';
-  //document.querySelector('input[name="search_term"]').value;
-  console.log(search_term);
-  asynchronousFunction(search_term);
+button.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    console.log('button pressed');
+
+    let search_term = document.querySelector('input[name="search_term"]').value;
+    console.log(search_term);
+    asynchronousFunction(search_term);
 });
